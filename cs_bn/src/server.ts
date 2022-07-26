b2b67675a5729780bc919408761b6207050442c0
afonso.lopocarvalho@gmail.com <afonso.lopocarvalho@gmail.com>
2022-07-26 23:27:09 +0100

    first push

---
+++ cs_bn/src/server.ts
@@ -0,0 +1,33 @@
+import helmet from "helmet";
+import express, { Request, Response, NextFunction } from "express";
+import morgan from "morgan";
+import cors, { CorsOptions } from "cors";
+
+const app = express();
+const corsOptions: CorsOptions = {
+  origin: process.env["ORIGIN"],
+  exposedHeaders: "Content-Disposition", //? https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Disposition
+};
+
+app.use(cors(corsOptions));
+app.use(morgan("dev"));
+app.use(express.json()); //? Json parser
+app.use(express.urlencoded({ extended: true })); //? Parses bodies
+app.use(helmet());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to CryptoStream" });
+});
+
+// ? Default error handler
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).send({
+    message: err.message,
+  });
+});
+
+//checks if there is an environment variable set, if not set to port 8080
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`The server is running on port ${PORT}`);
+});
